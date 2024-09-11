ac5e1c4b34d6250bd20e11dbc44e944da7717291
Thiery Laverdure <thiery@spacestud.io>
2024-09-11 08:10:11 -0400

    Add file system manager tests

---
+++ server/storage/file_system_manager_test.go
@@ -0,0 +1,47 @@
+package storage_test
+
+import (
+	"litebase/internal/test"
+	"litebase/server/storage"
+	"testing"
+)
+
+func TestLocalFS(t *testing.T) {
+	test.Run(t, func() {
+		fs := storage.LocalFS()
+
+		if fs == nil {
+			t.Error("LocalFS() returned nil")
+		}
+	})
+}
+
+func TestObjectFS(t *testing.T) {
+	test.Run(t, func() {
+		fs := storage.ObjectFS()
+
+		if fs == nil {
+			t.Error("ObjectFS() returned nil")
+		}
+	})
+}
+
+func TestTmpFS(t *testing.T) {
+	test.Run(t, func() {
+		fs := storage.TmpFS()
+
+		if fs == nil {
+			t.Error("TmpFS() returned nil")
+		}
+	})
+}
+
+func TestTieredFS(t *testing.T) {
+	test.Run(t, func() {
+		fs := storage.TieredFS()
+
+		if fs == nil {
+			t.Error("TieredFS() returned nil")
+		}
+	})
+}
