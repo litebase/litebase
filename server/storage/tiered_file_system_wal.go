1646d839212ad6c03c6f69ebb8bde82d371328cb
Thiery Laverdure <thiery@spacestud.io>
2025-05-27 21:07:24 -0400

    Remove dead code

--- server/storage/tiered_file_system_wal.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-/*
-The TieredFileSystemWal is a struct that represents a tiered file system
-write-ahead log. This write-ahead log is used to store mutations made
-to files in the tiered file system on the current node so they can
-be recovered in the event of a failure.
-*/
-type TieredFileSystemWal struct{}
