c33d543650779f845922ed04f129aaf930a475b7
Thiery Laverdure <thiery@spacestud.io>
2025-06-19 13:56:04 -0400

    Update node identifier to include a time of which the identifier was last updated for consensus

--- pkg/cluster/node_identifier.go
+++ pkg/cluster/node_identifier.go
@@ -1,13 +1,17 @@
 package cluster
 
+import "time"
+
 type NodeIdentifier struct {
-	Address string `json:"address"`
-	ID      string `json:"id,omitempty"`
+	Address  string    `json:"address"`
+	ID       string    `json:"id,omitempty"`
+	UpdateAt time.Time `json:"update_at"`
 }
 
-func NewNodeIdentifier(address string, id string) *NodeIdentifier {
+func NewNodeIdentifier(address string, id string, updateAt time.Time) *NodeIdentifier {
 	return &NodeIdentifier{
-		Address: address,
-		ID:      id,
+		Address:  address,
+		ID:       id,
+		UpdateAt: updateAt,
 	}
 }
--- pkg/cluster/node_identifier_test.go
+++ pkg/cluster/node_identifier_test.go
@@ -2,12 +2,13 @@ package cluster_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/litebase/litebase/pkg/cluster"
 )
 
 func TestNewNodeIdentifier(t *testing.T) {
-	identifier := cluster.NewNodeIdentifier("127.0.0.1:8080", "1")
+	identifier := cluster.NewNodeIdentifier("127.0.0.1:8080", "1", time.Now())
 
 	if identifier.Address != "127.0.0.1:8080" {
 		t.Error("Address not set correctly")
