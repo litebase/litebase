af5cc2c3ae6a526f6e8b6166e5dc6c51effe96d0
Thiery Laverdure <thiery@spacestud.io>
2025-06-24 12:29:35 -0400

    Add access key privilege to read

--- pkg/auth/access_key_privilege.go
+++ pkg/auth/access_key_privilege.go
@@ -4,5 +4,6 @@ const (
 	AccessKeyPrivilegeCreate Privilege = "access-key:create"
 	AccessKeyPrivilegeDelete Privilege = "access-key:delete"
 	AccessKeyPrivilegeList   Privilege = "access-key:list"
+	AccessKeyPrivilegeRead   Privilege = "access-key:read"
 	AccessKeyPrivilegeUpdate Privilege = "access-key:update"
 )
