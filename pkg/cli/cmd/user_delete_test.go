6832e30addc7ebc58f6580b79753448b95504e1c
Thiery Laverdure <thiery@spacestud.io>
2025-06-26 11:52:31 -0400

    Add user delete test

--- pkg/cli/cmd/user_delete_test.go
+++ pkg/cli/cmd/user_delete_test.go
@@ -1,1 +1,43 @@
 package cmd_test
+
+import (
+	"testing"
+
+	"github.com/litebase/litebase/internal/test"
+	"github.com/litebase/litebase/pkg/auth"
+)
+
+func TestUserDeleteCmd(t *testing.T) {
+	test.Run(t, func() {
+		server := test.NewTestServer(t)
+		defer server.Shutdown()
+
+		cli := test.NewTestCLI(server.App).
+			WithServer(server).
+			WithAccessKey([]auth.AccessKeyStatement{
+				{Effect: auth.AccessKeyEffectAllow, Resource: "*", Actions: []auth.Privilege{"*"}},
+			})
+
+		err := server.App.Auth.UserManager().Add(
+			"username",
+			"password",
+			[]auth.AccessKeyStatement{
+				{Effect: auth.AccessKeyEffectAllow, Resource: "*", Actions: []auth.Privilege{"*"}},
+			},
+		)
+
+		if err != nil {
+			t.Fatalf("failed to create user: %v", err)
+		}
+
+		err = cli.Run("user", "delete", "username")
+
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if !cli.ShouldSee("User deleted") {
+			t.Error("expected output to contain 'User deleted'")
+		}
+	})
+}
