9e377ca3a59916e47cc0746a4d38893b19deeb85
Thiery Laverdure <thiery@spacestud.io>
2025-12-16 10:59:12 -0500

    Add DatabaseExport and associated tests for range management

---
+++ pkg/database/database_export.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/litebase/litebase/pkg/storage"
+)
+
+type DatabaseExport struct {
+	ID          string
+	fileSystem  *storage.DurableDatabaseFileSystem
+	ranges      map[int64]storage.DataRangeIndexEntry
+	mutex       *sync.Mutex
+	StartedAt   time.Time
+	CompletedAt *time.Time
+}
+
+func NewDatabaseExport(fileSystem *storage.DurableDatabaseFileSystem, ranges map[int64]storage.DataRangeIndexEntry) *DatabaseExport {
+	return &DatabaseExport{
+		ID:         uuid.NewString(),
+		fileSystem: fileSystem,
+		ranges:     ranges,
+		mutex:      &sync.Mutex{},
+		StartedAt:  time.Now(),
+	}
+}
+
+func (de *DatabaseExport) End() {
+	de.mutex.Lock()
+	defer de.mutex.Unlock()
+
+	now := time.Now()
+	de.CompletedAt = &now
+}
+
+func (de *DatabaseExport) GetRange(rangeNumber int64) (*storage.Range, error) {
+	de.mutex.Lock()
+	defer de.mutex.Unlock()
+
+	// Check if the range exists in the export
+	if _, exists := de.ranges[rangeNumber]; !exists {
+		return nil, errors.New("range not found in export")
+	}
+
+	// Get the range from the file system
+	return de.fileSystem.GetRangeFile(rangeNumber)
+}
+
+func (de *DatabaseExport) RangeCount() int {
+	de.mutex.Lock()
+	defer de.mutex.Unlock()
+
+	return len(de.ranges)
+}
