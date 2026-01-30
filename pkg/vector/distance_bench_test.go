ad1bcac5b8ce99e84c96c7ebb54295608913475a
Thiery Laverdure <thiery@spacestud.io>
2026-01-30 10:55:12 -0500

    Refactor benchmarks to use vector package imports
    Changed the test package to 'vector_test' and updated all function calls to use explicit 'vector.' package references. This improves clarity and ensures benchmarks use the public API, aiding maintainability and test isolation.

--- pkg/vector/distance_bench_test.go
+++ pkg/vector/distance_bench_test.go
@@ -1,7 +1,9 @@
-package vector
+package vector_test
 
 import (
 	"testing"
+
+	"github.com/litebase/litebase/pkg/vector"
 )
 
 // Benchmark blob parsing
@@ -12,13 +14,13 @@ func BenchmarkParseVectorBlob(b *testing.B) {
 		data[i] = float32(i) * 0.1
 	}
 
-	blob, _ := EncodeFloat32(data)
+	blob, _ := vector.EncodeFloat32(data)
 
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		_, err := ParseVectorBlob(blob)
+		_, err := vector.ParseVectorBlob(blob)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -33,17 +35,17 @@ func BenchmarkParseVectorBlobPooled(b *testing.B) {
 		data[i] = float32(i) * 0.1
 	}
 
-	blob, _ := EncodeFloat32(data)
+	blob, _ := vector.EncodeFloat32(data)
 
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		vb, err := ParseVectorBlobPooled(blob)
+		vb, err := vector.ParseVectorBlobPooled(blob)
 		if err != nil {
 			b.Fatal(err)
 		}
-		PutVectorBlob(vb)
+		vector.PutVectorBlob(vb)
 	}
 }
 
@@ -58,17 +60,16 @@ func BenchmarkDistanceL2(b *testing.B) {
 		vec2[i] = float32(i) * 0.2
 	}
 
-	blob1, _ := EncodeFloat32(vec1)
-	blob2, _ := EncodeFloat32(vec2)
+	blob1, _ := vector.EncodeFloat32(vec1)
+	blob2, _ := vector.EncodeFloat32(vec2)
 
-	vb1, _ := ParseVectorBlob(blob1)
-	vb2, _ := ParseVectorBlob(blob2)
-
+	vb1, _ := vector.ParseVectorBlob(blob1)
+	vb2, _ := vector.ParseVectorBlob(blob2)
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		_, err := DistanceL2(vb1, vb2)
+		_, err := vector.DistanceL2(vb1, vb2)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -86,17 +87,17 @@ func BenchmarkDistanceCosine(b *testing.B) {
 		vec2[i] = float32(i) * 0.2
 	}
 
-	blob1, _ := EncodeFloat32(vec1)
-	blob2, _ := EncodeFloat32(vec2)
+	blob1, _ := vector.EncodeFloat32(vec1)
+	blob2, _ := vector.EncodeFloat32(vec2)
 
-	vb1, _ := ParseVectorBlob(blob1)
-	vb2, _ := ParseVectorBlob(blob2)
+	vb1, _ := vector.ParseVectorBlob(blob1)
+	vb2, _ := vector.ParseVectorBlob(blob2)
 
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		_, err := DistanceCosine(vb1, vb2)
+		_, err := vector.DistanceCosine(vb1, vb2)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -114,17 +115,16 @@ func BenchmarkDistanceDot(b *testing.B) {
 		vec2[i] = float32(i) * 0.2
 	}
 
-	blob1, _ := EncodeFloat32(vec1)
-	blob2, _ := EncodeFloat32(vec2)
+	blob1, _ := vector.EncodeFloat32(vec1)
+	blob2, _ := vector.EncodeFloat32(vec2)
 
-	vb1, _ := ParseVectorBlob(blob1)
-	vb2, _ := ParseVectorBlob(blob2)
-
+	vb1, _ := vector.ParseVectorBlob(blob1)
+	vb2, _ := vector.ParseVectorBlob(blob2)
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		_, err := DistanceDot(vb1, vb2)
+		_, err := vector.DistanceDot(vb1, vb2)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -142,16 +142,16 @@ func BenchmarkParseAndDistance(b *testing.B) {
 		vec2[i] = float32(i) * 0.2
 	}
 
-	blob1, _ := EncodeFloat32(vec1)
-	blob2, _ := EncodeFloat32(vec2)
+	blob1, _ := vector.EncodeFloat32(vec1)
+	blob2, _ := vector.EncodeFloat32(vec2)
 
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		vb1, _ := ParseVectorBlob(blob1)
-		vb2, _ := ParseVectorBlob(blob2)
-		_, err := DistanceL2(vb1, vb2)
+		vb1, _ := vector.ParseVectorBlob(blob1)
+		vb2, _ := vector.ParseVectorBlob(blob2)
+		_, err := vector.DistanceL2(vb1, vb2)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -161,7 +161,7 @@ func BenchmarkParseAndDistance(b *testing.B) {
 // Benchmark TopKHeap operations
 func BenchmarkTopKHeapInsert(b *testing.B) {
 	k := 10
-	heap := NewTopKHeap(k)
+	heap := vector.NewTopKHeap(k)
 
 	b.ResetTimer()
 	b.ReportAllocs()
@@ -176,18 +176,19 @@ func BenchmarkMergeHeaps(b *testing.B) {
 	k := 10
 	numHeaps := 40 // Simulating 40 workers (2x20 CPUs)
 
-	heaps := make([]*TopKHeap, numHeaps)
+	heaps := make([]*vector.TopKHeap, numHeaps)
+
 	for i := range heaps {
-		heaps[i] = NewTopKHeap(k)
-		for j := 0; j < 100; j++ {
+		heaps[i] = vector.NewTopKHeap(k)
+
+		for j := range 100 {
 			heaps[i].Insert(int64(i*100+j), float64(j))
 		}
 	}
 
-	b.ResetTimer()
 	b.ReportAllocs()
 
-	for i := 0; i < b.N; i++ {
-		_ = MergeHeaps(heaps, k)
+	for b.Loop() {
+		_ = vector.MergeHeaps(heaps, k)
 	}
 }
